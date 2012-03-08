8d9246ac01281306ae6ef58a903111f065436b4d
Stephen Figgins <stephen.figgins@gmail.com>
2012-03-08 13:45:13 -0600

    change project to use distutils.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    version='0.0.1',
+    description='Simple weblog application for Django',
+    author='Stephen Figgins',
+    author_email='stephen.figgins@gmail.com',
+    name='django-simplejournal',
+    url='https://github.com/sfiggins/django-simplejournal',
+    packages=['simplejournal'],
+    license='BSD License',
+    platforms=['OS Independent'],
+)
